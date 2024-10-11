e14a1f4ed7b9fc07a799a2d209d782313d033422
Arshiya nehal <143194035+arshiya-nehal@users.noreply.github.com>
2024-10-11 12:36:53 +0530

    Time: 10 ms (95.84%), Space: 27 MB (80.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //sort
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int freq=1,ans=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+        //(Moore's voting algorithm)
+        int freq=0,ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(ans==nums[i]){
                 freq++;
             }
             else{
-                freq=1;
-                ans=nums[i];
+                freq--;
             }
-            if(freq>n/2){
-                return ans;
-            }}
-        return ans;
         }
-        };
+        return ans;
+    }};
+       //(optimised way) 
+        //sort
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // int freq=1,ans=nums[0];
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         freq++;
+        //     }
+        //     else{
+        //         freq=1;
+        //         ans=nums[i];
+        //     }
+        //     if(freq>n/2){
+        //         return ans;
+        //     }}
+        // return ans;
+        // }
+        // };
+        //(brute force)
 //         int n=nums.size();
 //         for(int val:nums){
 //             int freq=0;
